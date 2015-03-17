b6a27c74707ab2ae48347663f6eef02f2b20f6ed
Filip Smets <alemaaltevinden@gmail.com>
2015-03-17 15:35:50 +0100

    Set xml files to moved files

--- src/game.cpp
+++ src/game.cpp
@@ -5,8 +5,8 @@
 int main() {
 	Room current_room;
 	
-	current_room.loadFromXMLFile("Speelveld1.0.xml");
-	current_room.loadMovesFromXMLFile("Bewegingen1.0.xml");
+	current_room.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	current_room.loadMovesFromXMLFile("xmlfiles/Bewegingen1.0.xml");
 	
 	current_room.executeAllMoves("HuidigSpeelveld.txt", "ResterendeBewegingen.txt");
 	
