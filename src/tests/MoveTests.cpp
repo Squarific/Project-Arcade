fb3a34bba1e8fed952cb52e1715404c34d454a62
Jens Bijtebier <jens.bijtebier@gmail.com>
2015-03-18 14:06:29 +0100

    Added a few tests

---
+++ src/tests/MoveTests.cpp
@@ -0,0 +1,43 @@
+// DUMMY TEST
+TEST(movetests, dummytest) {
+	EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+}
+
+// GET DIRECTIONS
+TEST(movetests, directioncheck) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	testroom.loadMovesFromXMLFile("xmlfiles/Bewegingen1.0.xml");
+	
+	EXPECT_EQ(testroom.moves[0].get_direction(), 1);
+	EXPECT_EQ(testroom.moves[1].get_direction(), 1);
+	EXPECT_EQ(testroom.moves[2].get_direction(), 2);
+	EXPECT_EQ(testroom.moves[3].get_direction(), 1);
+	EXPECT_EQ(testroom.moves[4].get_direction(), 2);
+	EXPECT_EQ(testroom.moves[5].get_direction(), 1);
+	EXPECT_EQ(testroom.moves[6].get_direction(), 0);
+}
+
+// GET NAMES
+TEST(movetests, namecheck) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	testroom.loadMovesFromXMLFile("xmlfiles/Bewegingen1.0.xml");
+	
+	for (int i = 0; i < 7; i++) {
+		EXPECT_EQ(testroom.moves[0].get_name(), "Chip")
+	}
+}
+
+// EXECUTE MOVES - CHECK PLAYER LOCATION
+TEST(movetests, movecheck) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	testroom.loadMovesFromXMLFile("xmlfiles/Bewegingen1.0.xml");
+	testroom.executeAllMoves("HuidigSpeelveld.txt", "ResterendeBewegingen.txt");
+	
+	EXPECT_EQ(testroom.get_instance(2, 8).get_type(), 1);
+	EXPECT_EQ(testroom.get_player_width(), 2);
+	EXPECT_EQ(testroom.get_player_height(), 8);
+}
---
+++ src/tests/RoomTests.cpp
@@ -0,0 +1,59 @@
+// DUMMY TEST
+TEST(roomtests, dummytest) {
+	EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+}
+
+// HEIGHT AND WIDTH - CORRECT
+TEST(roomtests, xmlheightwidthcheck) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	
+	EXPECT_EQ(testroom.get_width(), 10);
+	EXPECT_EQ(testroom.get_height(), 10);
+}
+
+// ROOM NAME
+TEST(roomtests, xmlnamecheck) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	
+	EXPECT_EQ(testroom.get_name(), "Level 1");
+}
+
+// INSTANCE TEST 1 - TYPES
+TEST(roomtests, xmlinstancecheck1) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	
+	EXPECT_EQ(testroom.get_instance(5, 5).get_type(), 0);
+	EXPECT_EQ(testroom.get_instance(4, 5).get_type(), 1);
+	EXPECT_EQ(testroom.get_instance(0, 0).get_type(), 2);
+	EXPECT_EQ(testroom.get_instance(5, 2).get_type(), 3);
+}
+
+// INSTANCE TEST 2 - NAMES
+TEST(roomtests, xmlinstancecheck2) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	
+	EXPECT_EQ(testroom.get_instance(4, 5).get_name(), "Chip");
+}
+
+// INSTANCE TEST 3 - MOVABLE
+TEST(roomtests, xmlinstancecheck3) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	
+	EXPECT_FALSE(testroom.get_moveable(0, 0).get_movable());
+	EXPECT_TRUE(testroom.get_moveable(3, 4).get_movable());
+}
+
+// PLAYER LOCATION TEST
+TEST(roomtests, xmlplayerlocationcheck) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+	
+	EXPECT_EQ(testroom.get_player_width(), 4);
+	EXPECT_EQ(testroom.get_player_height(), 5);
+}
