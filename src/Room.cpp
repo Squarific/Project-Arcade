3ffb51b8556170f44592c18d3734556058254e65
Filip Smets <alemaaltevinden@gmail.com>
2015-05-05 14:10:39 +0200

    Readded jens bullshit

--- src/Room.cpp
+++ src/Room.cpp
@@ -6,6 +6,88 @@
 #include "Instance.h"
 #include "DesignByContract.h"
 
+class Room {
+  private:
+	std::string name;
+	int width;
+	int height;
+	std::vector< std::vector< Instance*> > instances;
+	bool is_initialized;
+	
+  public:
+	std::vector< Move*> moves;
+
+	Room() {
+		width = 0;
+		height = 0;
+		is_initialized = false;
+	}
+	
+	Room(int w, int h, std::string n) {
+		width = w;
+		height = h;
+		name = n;
+		is_initialized = false;
+	}
+
+	~Room();
+	
+	// Prints
+	void print_dimensions();
+	void print_ascii();
+	void printRoom();
+
+	// Init
+	// Fills the room width empty instances. (type = 0, movable = false)
+	void init();
+	
+	// Height
+	void set_height(int h);
+	int get_height();
+	
+	// Width
+	void set_width(int w);
+	int get_width();
+	
+	// Name
+	void set_name(std::string str);
+	std::string get_name();
+	
+	// Instances
+	bool set_instance(int width, int height, int type);
+	bool set_instance(int width, int height, int type, std::string id);
+	Instance* get_instance(int width, int height);
+	void set_instance_name(int width, int height, std::string str);
+	
+	// Moves an instance from location 'from', to location 'to'
+	// The previous location will be filled with an empty instance.
+	void move_instance(int from_width, int from_height, int to_width, int to_height);
+	
+	// Executes a move command, moving the player and other adjacent objects depending on their movable status.
+	// Returns true if the move was succesfully executed, otherwise returns false.
+	bool execute_move(Move*& move);
+	
+	// Tries to execute all moves in the vector<move> moves.
+	// Should any of the moves return an error, the function will stop and write the room properties and remaining moves to an ascii file.
+	// If all moves are finished, the room properties will be written to an ascii file, as well as a file saying there are no remaining moves.
+	void executeAllMoves(const char* roomfilename, const char* movesfilename);
+
+	// Player Coordinates
+	int get_player_width();
+	int get_player_height();
+
+	// XML
+	bool loadFromXMLFile(const char* filename);
+	bool loadMovesFromXMLFile(const char* filename);
+	
+	// File Outpot
+	void writeToFile(const char* filename);
+	void writeMovesToFile(const char* filename);
+
+	// Delete Instances
+	void deleteInstances();
+};
+
 void Room::print_dimensions() {
 	std::cout << width << "x" << height << std::endl;
 }
