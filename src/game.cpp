549eb5c8ca4eea80026ffd38ce836c024abf4fd5
Filip Smets <alemaaltevinden@gmail.com>
2015-05-05 20:26:36 +0200

    Reversed html output

--- src/game.cpp
+++ src/game.cpp
@@ -8,19 +8,19 @@ int main(int argc, char *argv[]) {
 
 	if (argc < 3) {
 		if (argc == 1) {
-			std::cout << "No arguments given. Entering the developer testing grounds..." << std::endl;
+			// std::cout << "No arguments given. Entering the developer testing grounds..." << std::endl;
 
-			current_room.loadFromXMLFile("xmlfiles/Level3.xml");
-			current_room.loadMovesFromXMLFile("xmlfiles/Level3Acties.xml");
+			// current_room.loadFromXMLFile("xmlfiles/Level3.xml");
+			// current_room.loadMovesFromXMLFile("xmlfiles/Level3Acties.xml");
 
-			current_room.executeAllMoves("HuidigSpeelveld.txt", "ResterendeBewegingen.txt");
+			// current_room.executeAllMoves("HuidigSpeelveld.txt", "ResterendeBewegingen.txt");
 
-			current_room.printRoom(std::cout);
-/*
+			// current_room.printRoom(std::cout);
+
 			UserInterface ui;
 			ui.enterMenu(cin, cout);
-*/
-			std::cout << "Leaving developer testing grounds!\nCYA" << std::endl;
+
+			// std::cout << "Leaving developer testing grounds!\nCYA" << std::endl;
 
 			return 0;
 		}
