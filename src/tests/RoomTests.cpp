dbc629fb6c4bd7a09f33a84480ef0e0ef3df407e
Jens Bijtebier <jens.bijtebier@gmail.com>
2015-05-06 11:28:47 +0200

    hopefully fixed some test, added some tests

--- src/tests/RoomTests.cpp
+++ src/tests/RoomTests.cpp
@@ -22,15 +22,17 @@ TEST(roomtests, xmlnamecheck) {
 }
 
 // INSTANCE TEST 1 - TYPES
-// TEST(roomtests, xmlinstancecheck1) {
-// 	Room testroom;
-// 	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
+TEST(roomtests, xmlinstancecheck1) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Level3.xml");
 	
-// 	EXPECT_EQ(testroom.get_instance(5, 5)->get_type(), 0);
-// 	EXPECT_EQ(testroom.get_instance(4, 5)->get_type(), 1);
-// 	EXPECT_EQ(testroom.get_instance(0, 0)->get_type(), 2);
-// 	EXPECT_EQ(testroom.get_instance(5, 2)->get_type(), 3);
-// }
+	EXPECT_EQ(testroom.get_instance(0, 9)->get_type(), 1);
+	EXPECT_EQ(testroom.get_instance(2, 7)->get_type(), 2);
+	EXPECT_EQ(testroom.get_instance(2, 8)->get_type(), 3);
+	EXPECT_EQ(testroom.get_instance(5, 7)->get_type(), 5);
+	EXPECT_EQ(testroom.get_instance(8, 8)->get_type(), 6);
+	EXPECT_EQ(testroom.get_instance(5, 8)->get_type(), 7);
+}
 
 // INSTANCE TEST 2 - NAMES
 TEST(roomtests, xmlinstancecheck2) {
@@ -41,13 +43,13 @@ TEST(roomtests, xmlinstancecheck2) {
 }
 
 // INSTANCE TEST 3 - MOVABLE
-// TEST(roomtests, xmlinstancecheck3) {
-// 	Room testroom;
-// 	testroom.loadFromXMLFile("xmlfiles/Speelveld1.0.xml");
-	
-// 	EXPECT_FALSE(testroom.get_instance(0, 0)->get_movable());
-// 	EXPECT_TRUE(testroom.get_instance(3, 4)->get_movable());
-// }
+TEST(roomtests, xmlinstancecheck3) {
+	Room testroom;
+	testroom.loadFromXMLFile("xmlfiles/Level3.xml");
+
+	EXPECT_FALSE(testroom.get_instance(6, 8)->get_movable());
+	EXPECT_TRUE(testroom.get_instance(2, 7)->get_movable());
+}
 
 // PLAYER LOCATION TEST
 TEST(roomtests, xmlplayerlocationcheck) {
