bcaed5a42f82cc94416470057acc382444604796
Filip Smets <alemaaltevinden@gmail.com>
2015-05-06 11:08:52 +0200

    Add userinterfacetests

--- src/tests/UserInterfaceTests.cpp
+++ src/tests/UserInterfaceTests.cpp
@@ -1,3 +1,10 @@
-TEST(userinterfacetest, helpCommand) {
-	
+TEST(userinterfacetest, printHelp) {
+	UserInterface ui;
+
+	std::ofstream file;
+	file.open("printhelp_temp_file.txt");
+
+	ui.printHelp(file);
+
+	FileCompare("printhelp_expected.txt", "printhelp_temp_file.txt");
 }
